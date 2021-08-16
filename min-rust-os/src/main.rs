60c9fa6ba1ca9dd98e436e71f5da8f498c5c4fb3
karencfv <karencfv@gmail.com>
2021-08-16 18:38:12 +1200

    Implement printing to VGA buffer tests

--- min-rust-os/src/main.rs
+++ min-rust-os/src/main.rs
@@ -143,8 +143,3 @@ fn test_runner(tests: &[&dyn Testable]) {
 
     exit_qemu(QemuExitCode::Success);
 }
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
--- min-rust-os/src/vga_buffer.rs
+++ min-rust-os/src/vga_buffer.rs
@@ -178,3 +178,28 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    // The function defines a test string, prints it using println, and then iterates over the screen
+    // characters of the static WRITER, which represents the vga text buffer. Since println prints to the
+    // last screen line and then immediately appends a newline, the string should appear on line BUFFER_HEIGHT - 2
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
