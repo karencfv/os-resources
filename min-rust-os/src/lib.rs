58bd21147aaf10128a76812a358996c23658b2bd
karencfv <karencfv@gmail.com>
2021-08-16 19:49:32 +1200

    Integration tests

---
+++ min-rust-os/src/lib.rs
@@ -0,0 +1,85 @@
+// Like the main.rs, the lib.rs is a special file that is automatically recognized by cargo.
+// The library is a separate compilation unit, so we need to specify the #![no_std] attribute again.
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
+
+pub mod serial;
+pub mod vga_buffer;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        // \t is the tab character
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+// `&[&dyn Fn()/Testable]` is a list of references to types that can be called like a function
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+
+    // Note that we still need an endless loop after the exit_qemu call because the compiler
+    // does not know that the isa-debug-exit device causes a program exit.
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    // We use exit code 0x10 for success and 0x11 for failure. The actual exit codes do not matter much,
+    // as long as they don't clash with the default exit codes of QEMU. For example, using exit code 0
+    // for success is not a good idea because it becomes (0 << 1) | 1 = 1
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    // Using unsafe as writing to an I/O port can generally result in arbitrary behaviour.
+    unsafe {
+        // Instead of manually invoking the in and out assembly instructions, the x86_64 crate is used.
+        // The function creates a new Port at 0xf4, which is the iobase of the isa-debug-exit device.
+        // Then it writes the passed exit code to the port. We use u32 because we specified the iosize
+        // of the isa-debug-exit device as 4 bytes.
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
--- min-rust-os/src/main.rs
+++ min-rust-os/src/main.rs
@@ -6,16 +6,14 @@
 #![no_main]
 // Implement a custom testing framework for the kernel
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(min_rust_os::test_runner)]
 // The kernel uses _start as the entry point instead of main.
 // It's necessary to change the name of the generated function to
 // something different than main through the reexport_test_harness_main attribute.
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use x86_64::instructions::port::Port;
 
-mod serial;
 mod vga_buffer;
 
 // Since the standard library has been removed, Some functionality needs to be implemented manually.
@@ -35,13 +33,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failure);
-
-    // Note that we still need an endless loop after the exit_qemu call because the compiler
-    // does not know that the isa-debug-exit device causes a program exit.
-    loop {}
+    min_rust_os::test_panic_handler(info)
 }
 
 static HELLO: &[u8] = b"Hi from the minimal OS :)";
@@ -53,6 +45,7 @@ static HELLO: &[u8] = b"Hi from the minimal OS :)";
 pub extern "C" fn _start() -> ! {
     // vga_buffer::print_something();
 
+    // Write to BGA buffer directly
     use core::fmt::Write;
     vga_buffer::WRITER
         .lock()
@@ -93,53 +86,4 @@ pub extern "C" fn _start() -> ! {
 
     // Uncomment loop if panic removed
     loop {}
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        // \t is the tab character
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    // We use exit code 0x10 for success and 0x11 for failure. The actual exit codes do not matter much,
-    // as long as they don't clash with the default exit codes of QEMU. For example, using exit code 0
-    // for success is not a good idea because it becomes (0 << 1) | 1 = 1
-    Success = 0x10,
-    Failure = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    // Using unsafe as writing to an I/O port can generally result in arbitrary behaviour.
-    unsafe {
-        // Instead of manually invoking the in and out assembly instructions, the x86_64 crate is used.
-        // The function creates a new Port at 0xf4, which is the iobase of the isa-debug-exit device.
-        // Then it writes the passed exit code to the port. We use u32 because we specified the iosize
-        // of the isa-debug-exit device as 4 bytes.
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-// `&[&dyn Fn()]` is a list of references to types that can be called like a function
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
 }
